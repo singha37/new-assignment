3fc210d8b6ad6cb9110f535a62a06a7561676916
singha37 <singha37@g.toiohomai.ac.nz>
2018-04-14 12:28:26 +1200

    initial commit

---
+++ java/script.js
@@ -0,0 +1,69 @@
+function ValidateContactForm()
+{
+    var name = document.ContactForm.Name;
+    var email = document.ContactForm.Email;
+    var phone = document.ContactForm.Telephone;
+    var nocall = document.ContactForm.DoNotCall;
+    var what = document.ContactForm.Subject;
+    var comment = document.ContactForm.Comment;
+
+    if (name.value == "")
+    {
+        window.alert("Please enter your name.");
+        name.focus();
+        return false;
+    }
+    
+    if (email.value == "")
+    {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+    if (email.value.indexOf("@", 0) < 0)
+    {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+    if (email.value.indexOf(".", 0) < 0)
+    {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+
+    if ((nocall.checked == false) && (phone.value == ""))
+    {
+        window.alert("Please enter your telephone number.");
+        phone.focus();
+        return false;
+    }
+
+    if (what.selectedIndex < 1)
+    {
+        alert("Please tell us how we can help you.");
+        what.focus();
+        return false;
+    }
+
+    if (comment.value == "")
+    {
+        window.alert("Please provide a detailed description or comment.");
+        comment.focus();
+        return false;
+    }
+    return true;
+}
+
+function EnableDisable(chkbx)
+{
+    if(chkbx.checked == true)
+    {
+        document.ContactForm.Telephone.disabled = true;
+    }
+    else
+    {
+        document.ContactForm.Telephone.disabled = false;
+    }
+}
